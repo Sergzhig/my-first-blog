689897552ca39d40e615134df841ad7a6d79a89d
Sergzhig <zse1@bk.ru>
2020-04-28 23:20:24 +0300

    My django app commit2 task1 task2

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('task1/', views.index,name='task_1'),
+    path('task2/', views.index_ren,name='task_2'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Post
 from django.utils import timezone
+
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def index(request):
+    print(request.GET)
+    name=request.GET.get('name')
+    type = request.GET.get('type')
+    humanoid = request.GET.get('humanoid')
+    #print(name)
+    #return HttpResponse('Hello, Ernest')
+    if humanoid =="ДА":
+        s_hum='ты человечище!'
+    else:
+        s_hum='жаль, что ты не человек!'
+
+    return HttpResponse('<h> Привет, %s %s,- %s </h>'%(type,name,s_hum))
+
+def index_ren(request):
+    name = request.GET.get('name')
+    type = request.GET.get('type')
+    name_length = len(name)
+    context = {
+        "name": name,
+        "name_length": name_length,
+        "type": type
+    }
+    return render(request, 'index.html', context=context)
